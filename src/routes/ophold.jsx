b9625cb0828265f9fa844e2d7bf8d5547d32e3d3
joeykoldingkristensen <joeyk666@hotmail.com>
2025-05-07 12:48:15 +0200

    stay update

--- src/routes/ophold.jsx
+++ src/routes/ophold.jsx
@@ -1,9 +1,10 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { Stay } from "../components/stay/Stay";
 
 export const Route = createFileRoute("/ophold")({
     component: RouteComponent,
 });
 
 function RouteComponent() {
-    return <div>Hello "/ophold"!</div>;
+    return Stay();
 }
