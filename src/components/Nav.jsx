643330c8fbc6109250e60438bc09f5c44c6a11db
Lucas Täkker <50625016+Sniperhund@users.noreply.github.com>
2025-05-07 10:39:48 +0200

    More nav

--- src/components/Nav.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./Nav.module.scss"
-
-export const Nav = ({ homePage }) => {
-    return <nav>
-        <button className={styles.icon}>
-            <i className="fa-solid fa-bars" />
-        </button>
-    </nav>
-}
---
+++ src/components/Nav.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react"
+import styles from "./Nav.module.scss"
+import { Link } from "@tanstack/react-router"
+
+export const Nav = ({ homePage }: { homePage: boolean }) => {
+    const [open, setOpen] = useState(false)
+
+    return <nav>
+        <button className={styles.icon} onClick={() => setOpen(true)}>
+            <i className="fa-solid fa-bars" />
+        </button>
+
+        <ul className={`${styles.nav} ${open && styles.active}`}>
+            <button className={styles.close} onClick={() => setOpen(false)}>
+                <i className="fa-solid fa-xmark" />
+            </button>
+            <li>
+                <Link to="/ophold">Ophold</Link>
+            </li>
+            <li>
+                <Link to="/kontakt">Kontakt</Link>
+            </li>
+            <li>
+                <Link to="/aktiviter">Aktiviter</Link>
+            </li>
+            <li>
+                <Link to="/min-liste">Min Liste</Link>
+            </li>
+            <li>
+                <Link to="/backoffice">Backoffice</Link>
+            </li>
+        </ul>
+    </nav>
+}
