4f697798e87ca1f737f9e10a6ef7eab454a85576
Lucas Täkker <50625016+Sniperhund@users.noreply.github.com>
2025-05-07 11:58:06 +0200

    Added vite-tsconfig-paths

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@ import { defineConfig } from 'vite'
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite"
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import tsconfigPaths from "vite-tsconfig-paths"
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [
+        tsconfigPaths(),
         TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
         react(),
         tailwindcss()
