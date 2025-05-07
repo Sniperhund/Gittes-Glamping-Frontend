aacbc130326b427021f6c128fdaf75cdea461fdd
Lucas Täkker <50625016+Sniperhund@users.noreply.github.com>
2025-05-07 10:27:23 +0200

    Added font families

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createRouter, RouterProvider } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
+import WebFont from 'webfontloader'
 
 const router = createRouter({
     routeTree,
     defaultPreload: "intent",
     scrollRestoration: true
+})
+
+WebFont.load({
+    google: {
+        families: ["Zen Loop", "NanumGothic"]
+    }
 })
 
 createRoot(document.getElementById('root')).render(
