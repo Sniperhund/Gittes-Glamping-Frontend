0d294708ec3882745d83ccbbe879d82b2492d3b7
Lucas Täkker <50625016+Sniperhund@users.noreply.github.com>
2025-05-07 11:57:42 +0200

    Added test hero

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,8 +1,10 @@
 import { createFileRoute } from "@tanstack/react-router";
+import test from "../assets/image_00.jpg"
 
 const Home = () => {
-    return <>
-    </>
+    return <section className="hero">
+        <img src={test} alt="" />
+    </section>
 }
 
 export const Route = createFileRoute("/")({
