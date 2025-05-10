2b68cc06015c856397c6741c689939717abe2504
Lucas Täkker <50625016+Sniperhund@users.noreply.github.com>
2025-05-10 11:57:05 +0200

    Added last styles to nav and made it close after navigation

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,17 +1,29 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import styles from "./Nav.module.scss"
-import { Link } from "@tanstack/react-router"
+import { Link, useRouter } from "@tanstack/react-router"
 import logo from "../assets/logo.png"
 
 export const Nav = ({ homePage }: { homePage: boolean }) => {
     const [open, setOpen] = useState(false)
+    
+    const router = useRouter()
+
+    useEffect(() => {
+        const unsubscribe = router.subscribe("onRendered", () => {
+            setOpen(false)
+        })
+
+        return () => unsubscribe()
+    }, [])
 
     return <nav>
-        {homePage && <img className={styles.logo} src={logo} alt="Logo" />}
+        <div>
+            {!homePage ? <Link to="/"><img className={styles.logo} src={logo} alt="Logo" /></Link>: <div />}
 
-        <button className={styles.icon} onClick={() => setOpen(true)}>
-            <i className="fa-solid fa-bars" />
-        </button>
+            <button className={styles.icon} onClick={() => setOpen(true)}>
+                <i className="fa-solid fa-bars" />
+            </button>
+        </div>
 
         <ul className={`${styles.nav} ${open && styles.active}`}>
             <button className={styles.close} onClick={() => setOpen(false)}>
